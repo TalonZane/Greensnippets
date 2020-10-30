59c63be84e1f74b72defea82aa83cf4e3c942a35
TalonZane <62534258+TalonZane@users.noreply.github.com>
2020-10-30 15:46:17 -0500

    Create Unlock on Kill.cs

---
+++ Unlock on Kill.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mod
+{
+    class UnlockManager : MonoBehaviour
+    {
+        public static List<Modification> Unlockables = new List<Modification>();
+        public static void Unlock(string unlock)
+        {
+
+            for (int i = 0; i < Unlockables.Count; i++)
+            {
+                if (Unlockables[i].NameOverride == (unlock + Mod.ModTag))
+                {
+                    if (PlayerPrefs.GetInt(Unlockables[i].NameOverride, 0) == 1) return;
+
+                    PlayerPrefs.SetInt(Unlockables[i].NameOverride, 1);
+                    ModAPI.Register(Unlockables[i]);
+                    ModAPI.Notify("<i>Unlocked: " + Unlockables[i].NameOverride.TrimEnd(Mod.ModTag.ToCharArray()) + " in " + Unlockables[i].CategoryOverride.name + "!</i>");
+                    PlayerPrefs.SetInt("UnlockedItems", PlayerPrefs.GetInt("UnlockedItems", 0) + 1);
+                    ModAPI.Notify("<i>" + PlayerPrefs.GetInt("UnlockedItems", 0) + "/" + Unlockables.Count + "</i>");
+
+                }
+            }
+
+        }
+
+        public static void InitialPopulate()
+        {
+            for (int i = 0; i < Unlockables.Count; i++)
+            {
+                if (PlayerPrefs.GetInt(Unlockables[i].NameOverride, 0) == 1)
+                {
+                    ModAPI.Register(Unlockables[i]);
+                }
+            }
+        }
+    }
+}
